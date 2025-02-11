b8d7b3ce1caca8b08c48e89b07a8917c422ead3e
FTXPart2 <wesleychung98@gmail.com>
2025-02-10 21:25:49 -0800

    New commit

--- Item.java
+++ Item.java
@@ -1,3 +1,30 @@
-public class Item {
-    
+public class Item implements Comparable<Item> {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        if (name.length() > 7) {
+            throw new IllegalArgumentException("Name must be at most 7 characters long");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public int compareTo(Item other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + price + ")";
+    }
 }
